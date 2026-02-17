05c59fa620145b3cdacdb098feefae7e7f9eb5ba
jacob-s-codes <jacobbshaul@gmail.com>
2026-02-16 21:18:08 -0800

    changed logo to have animation

--- src/app/components/Home/Navbar.tsx
+++ src/app/components/Home/Navbar.tsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <div className='w-full z-30 top-0 sticky bg-black flex flex-row items-center justify-between px-12 py-4 text-white'>
         <div className='w-20 h-auto'>
-            <a href="/" className='text-3xl text-transparent bg-clip-text bg-gradient-to-r from-pinkaccent to-orangeaccent'>24Live</a>
+            <a href="/" className='text-3xl text-transparent bg-clip-text bg-gradient-to-r from-pinkaccent to-orangeaccent introtext'>24Live</a>
         </div>
         <div className='flex flex-row items-center justify-center gap-x-12 uppercase text-xl'>
             <a href="/play" className='hover:text-orangeaccent'>Play</a>
